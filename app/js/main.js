87f4d49d48558adcd48a653901cc057678ace018
max-tsymba <young.bezero@gmail.com>
2021-04-07 20:52:52 +0300

    Added Team section

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,12 @@ $(function(){
         perturbance: 0.01,
     });
 
+    // $('.team__content-slider').slick({
+    //     arrows: false,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     prevArrow: '<button class="slider-btn slider-btn__left"><img src="./img/arrow-left.png" alt=""></button>',
+    //     nextArrow: '<button class="slider-btn slider-btn__right"><img src="./img/arrow-right.png" alt=""></button>'
+    // });
+
 });
