5e01a44eb71bde32ac61a83ba9d9d82949eeeaad
max-tsymba <young.bezero@gmail.com>
2021-04-08 20:20:16 +0300

    added website intro

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,50 @@
+// Preloader Screen
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.intro__logo');
+let logoSpan = document.querySelectorAll('.intro__logo-elem');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        document.documentElement.style.overflowY = 'hidden';
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx+1) * 400);
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1) * 50);
+
+            });
+        }, 2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300);
+
+        setTimeout(()=>{
+            document.documentElement.style.overflowY = 'scroll';
+        }, 5000);
+    });
+
+});
+
+
 $(function(){
 
     $('.header').ripples({
         dropRadius: 10,
         perturbance: 0.01,
     });
+
 
     // $('.team__content-slider').slick({
     //     arrows: false,
