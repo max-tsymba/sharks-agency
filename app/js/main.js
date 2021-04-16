f44d30b0b2fc563a78d472b878270d255a9bd1c3
max-tsymba <young.bezero@gmail.com>
2021-04-16 20:46:35 +0300

    Changed Team Slider

--- app/js/main.js
+++ app/js/main.js
@@ -15,14 +15,37 @@ $(function(){
         });
     }, 4500)
 
+    $('.slider-center').slick({
 
-//     // $('.team__content-slider').slick({
-//     //     arrows: false,
-//     //     slidesToShow: 3,
-//     //     slidesToScroll: 1,
-//     //     prevArrow: '<button class="slider-btn slider-btn__left"><img src="./img/arrow-left.png" alt=""></button>',
-//     //     nextArrow: '<button class="slider-btn slider-btn__right"><img src="./img/arrow-right.png" alt=""></button>'
-//     // });
+        autoplay: true,
+        speed: 2000,
+        dots: false,
+        infinite: false,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                infinite: true
+            }
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                dots: true
+            }
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick"
+        }]
+    });
 
 });
 
@@ -149,6 +172,7 @@ function circleText() {
         Element[i].style.transform = "rotate("+i*22+"deg)"; 
     }
 }
+
 
 // MODULES-----------------------------------------------------------------------------------------------------------------
 function linkPageTranslating(WRAPPER) {
